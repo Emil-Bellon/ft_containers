33c8bac62cac86f27c6b285cd22cc67b9006cd61
Emil Bellon <ebellon@z1r7p1.42lyon.fr>
2022-11-25 18:32:20 +0100

    fix iterator

--- vector.hpp
+++ vector.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #include <iterator>
-#include "vector_iterator.hpp"
+#include "iterator.hpp"
 
 namespace ft
 {
@@ -9,18 +9,18 @@ namespace ft
 	{
 		public :
 
-			typedef				T										value_type;
-			typedef				Alloc									allocator_type;
-			typedef typename	allocator_type::reference				reference;
-			typedef typename	allocator_type::const_reference			const_reference;
-			typedef typename	allocator_type::pointer					pointer;
-			typedef typename	allocator_type::const_pointer			const_pointer;
-			typedef				ft::vector_iterator<pointer>			iterator;
-			typedef				ft::vector_iterator<const_pointer>		const_iterator;
-			typedef				std::reverse_iterator<iterator>			reverse_iterator;
-			typedef				std::reverse_iterator<const_iterator>	const_reverse_iterator;
-			typedef				std::ptrdiff_t							difference_type;
-			typedef				std::size_t								size_type;
+			typedef				T											value_type;
+			typedef				Alloc										allocator_type;
+			typedef typename	allocator_type::reference					reference;
+			typedef typename	allocator_type::const_reference				const_reference;
+			typedef typename	allocator_type::pointer						pointer;
+			typedef typename	allocator_type::const_pointer				const_pointer;
+			typedef				ft::random_access_iterator<pointer>			iterator;
+			typedef				ft::random_access_iterator<const_pointer>	const_iterator;
+			typedef				ft::reverse_iterator<iterator>				reverse_iterator;
+			typedef				ft::reverse_iterator<const_iterator>		const_reverse_iterator;
+			typedef				std::ptrdiff_t								difference_type;
+			typedef				std::size_t									size_type;
 
 		private :
 
